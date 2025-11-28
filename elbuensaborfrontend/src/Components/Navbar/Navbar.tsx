45bd73d6de1fb9d43e8a6b4d8a89babbaf7a3f6f
celinalopez <celina.lopez2799@gmail.com>
2025-11-28 01:44:06 -0300

    add titulo dropdown muestra usuario

--- elbuensaborfrontend/src/Components/Navbar/Navbar.tsx
+++ elbuensaborfrontend/src/Components/Navbar/Navbar.tsx
@@ -4,56 +4,58 @@ import "./navbar.css";
 import { useUser } from "../../contexts/UsuarioContext";
 
 export default function MyNavbar() {
-  const { user, logout } = useUser();
+    const { user, logout } = useUser();
 
-  return (
-    <Navbar expand="lg" className="navbar-container">
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          EL BUEN SABOR
-        </Navbar.Brand>
+    const dropdownTitle = user ? user.username : "Cuenta";
 
-        <Navbar.Toggle aria-controls="main-navbar" />
+    return (
+        <Navbar expand="lg" className="navbar-container">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    EL BUEN SABOR
+                </Navbar.Brand>
 
-        <Navbar.Collapse id="main-navbar">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
-              Inicio
-            </Nav.Link>
-            <Nav.Link as={Link} to="/productos">
-              Productos
-            </Nav.Link>
-            {(user?.role === "ADMIN" || user?.role === "EMPLEADO") && (
-              <Nav.Link as={Link} to="/dashboard/home">
-                Panel de administración
-              </Nav.Link>
-            )}
+                <Navbar.Toggle aria-controls="main-navbar" />
 
-            <NavDropdown title="Cuenta" id="basic-nav-dropdown">
-              {!user ? (
-                <>
-                  <NavDropdown.Item as={Link} to="/login">
-                    Ingresar
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/register">
-                    Registrarse
-                  </NavDropdown.Item>
-                </>
-              ) : (
-                <>
-                  <NavDropdown.Item as={Link} to="/account">
-                    Perfil
-                  </NavDropdown.Item>
-                  <hr />
-                  <NavDropdown.Item onClick={logout}>
-                    Cerrar sesión
-                  </NavDropdown.Item>
-                </>
-              )}
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+                <Navbar.Collapse id="main-navbar">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/">
+                            Inicio
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/productos">
+                            Productos
+                        </Nav.Link>
+                        {(user?.role === "ADMIN" || user?.role === "EMPLEADO") && (
+                            <Nav.Link as={Link} to="/dashboard/home">
+                                Panel de administración
+                            </Nav.Link>
+                        )}
+
+                        <NavDropdown title={dropdownTitle} id="basic-nav-dropdown">
+                            {!user ? (
+                                <>
+                                    <NavDropdown.Item as={Link} to="/login">
+                                        Ingresar
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item as={Link} to="/register">
+                                        Registrarse
+                                    </NavDropdown.Item>
+                                </>
+                            ) : (
+                                <>
+                                    <NavDropdown.Item as={Link} to="/account">
+                                        Perfil
+                                    </NavDropdown.Item>
+                                    <hr />
+                                    <NavDropdown.Item onClick={logout}>
+                                        Cerrar sesión
+                                    </NavDropdown.Item>
+                                </>
+                            )}
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
