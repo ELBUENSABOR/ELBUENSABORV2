bfc3966e87463b369a44e6bbb76fa688c865496c
celinalopez <celina.lopez2799@gmail.com>
2026-02-23 16:24:43 -0300

    test deploy

--- elbuensaborbackend/src/main/java/com/utn/elbuensabor/config/SecurityConfig.java
+++ elbuensaborbackend/src/main/java/com/utn/elbuensabor/config/SecurityConfig.java
@@ -84,6 +84,7 @@ public class SecurityConfig {
 
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // preflight
+                .requestMatchers("/", "/health", "/actuator/health").permitAll()
                 .requestMatchers("/api/auth/register").permitAll()
                 .requestMatchers("/api/auth/login").permitAll()
                 .requestMatchers("/api/auth/google").permitAll()
